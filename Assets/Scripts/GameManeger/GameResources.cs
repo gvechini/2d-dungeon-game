71ee6b0c827731330a181b6d9fa945dbee0b352d
Gabriela Vechini <gabriela.vechini@gmail.com>
2023-03-08 21:21:26 -0300

    create pop up list node types

---
+++ Assets/Scripts/GameManeger/GameResources.cs
@@ -0,0 +1,32 @@
+using Mono.Cecil;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameResources : MonoBehaviour
+{
+   private static GameResources instance;
+
+    public static GameResources Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<GameResources>("GameResources");
+            }
+            return instance;
+        }
+    }
+
+    #region Header DUNGEON
+    [Space(10)]
+    [Header("DUNGEON")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Populate with the dungeon RoomNodeTypeSO")]
+    #endregion
+
+    public RoomNodeTypeListSO roomNodeTypeList;
+}
